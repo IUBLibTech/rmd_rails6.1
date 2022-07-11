15b4b2a987f5e8e69baa7c3566b3b314dd057645
jaalbrec <jaalbrec@indiana.edu>
2022-07-11 10:48:20 -0400

    Reverted starting the delayed_job AtomFeed and JSON reader tasks to be called from rake tasks. These are no longer being initialized in config/initializers/delayed_job_config.rb

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,5 +1,3 @@
-require AfrHelper, AtomFeedReaderTask, AvalonItemsHelper, JsonReaderHelper, JsonReaderTask
-
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 15
 Delayed::Worker.max_attempts = 2
@@ -9,6 +7,3 @@ Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
-
-AtomFeedReaderTask.schedule!
-JsonReaderTask.schedule!
---
+++ lib/tasks/avalon_tasks.rake
@@ -0,0 +1,13 @@
+namespace :avalon do
+  desc 'Runs the background process that reads the Avalon atom feed'
+  task read_atom_feed: :environment do
+    # schedules the atom feed reader background process to run
+    AtomFeedReaderTask.schedule!
+  end
+
+  desc 'Runs the background process that parses JSON records in MCO'
+  task parse_json: :environment do
+    # schedules the JSON reader background process to run
+    JsonReaderTask.schedule!
+  end
+end
